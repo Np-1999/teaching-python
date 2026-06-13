8d130a2b75a876beb61c21f1744a9923339d05b2
Nikhil <nikhilpadole2014@gmail.com>
2026-06-13 18:42:43 +0530

    organize better

---
+++ calc/add.py
@@ -0,0 +1,10 @@
+import logging
+def add(a, b):
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    handler = logging.FileHandler(f'{__name__}.log')
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.info(f"Starting add run with a= {a}, b={b}")
+    return a + b
