00eb719903dca760c01008a297a8c2261011b65f
Nikhil <nikhilpadole2014@gmail.com>
2026-06-27 21:15:19 +0530

    Generator and pytest

---
+++ lesson3-generator-mock-pytest/pytest-mock/main.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_weather(city):
+    response = requests.get(f"https://api.weather.com/v1/{city}/")
+
+    if response.status_code == 200:
+        print("response code 200")
+        return response.json()
+    else:
+        raise ValueError
---
+++ lesson3-generator-mock-pytest/pytest-mock/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from main import get_weather
+
+def test_get_weather(mocker):
+    
+    mock_get = mocker.patch("main.requests.get")
+
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {"temperature": 25, "condition":"Sunny"}
+
+    result = get_weather("chicago")
+
+    assert result == {"temperature": 25, "condition":"Sunny"}
+    mock_get.assert_called_once_with("https://api.weather.com/v1/chicago/")
+
